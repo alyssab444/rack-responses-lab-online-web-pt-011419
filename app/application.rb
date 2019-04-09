cc671636e44806771f4993ef4930e2263e22911f
Alyssa Brown <alyssa.brown@live.com>
2019-04-09 01:47:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,15 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    if Time.now == 
+    if Time.now >= 12 
+      resp.write "Good Afternoon"
+      
+      now = Time.now
+
+     if now.hour >= 12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
     
     resp.write "Hello, World"
     resp.finish
